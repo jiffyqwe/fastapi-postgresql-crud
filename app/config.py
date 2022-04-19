d833fc7dfe7e1f94c19af94053974e645188615b
jiffyqwe <jiffyqwe123@gmail.com>
2022-04-19 15:25:47 +0300

    dot

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from env import settings
+from .env import settings
 
 DATABASE_URL = f"postgresql://{settings['POSTGRES_USER']}:{settings['POSTGRES_PASSWORD']}@{settings['POSTGRES_SERVER']}:{settings['POSTGRES_PORT']}/{settings['POSTGRES_DB']}"
 
